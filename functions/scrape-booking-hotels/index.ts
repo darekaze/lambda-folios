ccc441661e29b3b04360d1d084cbf958122cbd71
darekaze <darekaze@gmail.com>
2020-01-05 23:51:59 +0800

    in good progress

--- functions/scrape-booking-hotels/index.ts
+++ functions/scrape-booking-hotels/index.ts
@@ -1,4 +1,5 @@
 import chromium from 'chrome-aws-lambda'
+import dayjs from 'dayjs'
 import { CloudEventsContext } from '@google-cloud/functions-framework'
 import { Browser } from 'puppeteer-core'
 
@@ -9,46 +10,76 @@ interface PubSubMessage {
 
 //* Note: everything should be in this file
 export const scrapeBookingHotels = async (data: PubSubMessage, context: CloudEventsContext) => {
-  const browser: Browser = await chromium.puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,
-    headless: chromium.headless, // default to true in dev, set to false if it's too anoyying
-  })
+  const dateTimeNow = dayjs()
+  let browser: Browser = null
 
-  const page = await browser.newPage()
+  try {
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless, // default to true in dev, set to false if it's too anoyying
+    })
 
-  // TODO: setup custom user agent pls
-  // TODO: and set interrupt (don't load font and image)
+    const page = await browser.newPage()
 
-  await page.goto('https://www.booking.com/')
+    // TODO: setup custom user agent pls
+    // TODO: and set interrupt (don't load font and image)
 
-  // Booking.com prevent direct dom manipulation, so we type and click lol
-  await page.type('input[name=ss]', 'Tokyo')
+    await page.goto('https://www.booking.com/?selected_currency=JPY')
 
-  // TODO: make select date dynamic input
-  await page.click('.xp__date-time')
-  await page.click(`td[data-date="2020-01-06"]`)
-  await page.click(`td[data-date="2020-01-07"]`)
+    // Booking.com prevent direct dom manipulation, so we type and click lol
+    await page.type('input[name=ss]', 'Tokyo') // TODO: dynamic input destination
+    await page.click('.xp__date-time')
+    await page.click(`td[data-date="${dateTimeNow.format('YYYY-MM-DD')}"]`)
+    await page.click(`td[data-date="${dateTimeNow.add(1, 'day').format('YYYY-MM-DD')}"]`)
 
-  // submit
-  await page.click('button[data-sb-id="main"]')
+    // Submit and wait for new page to load
+    await Promise.all([
+      page.click('button[data-sb-id="main"]'),
+      page.waitForNavigation({ waitUntil: 'networkidle2' }),
+    ])
 
-  // Entering new page
-  await page.waitForSelector('#hotellist_inner')
+    // Set filters
+    await Promise.all([
+      page.click('#filter_out_of_stock a'),
+      page.click('#filter_concise_unit_type a[data-value="Hotels + more"]'),
+      page.waitForNavigation({ waitUntil: 'networkidle0' }), // kinda hacky?
+    ])
 
-  // await page.screenshot({ path: 'sc.png' })
+    // TODO: Start scraping (loop should start here)
+    const res = await page.evaluate(async () => {
+      const items = Array.from<HTMLElement>(document.querySelectorAll('#hotellist_inner .sr_item'))
+      const currentDate = new Date()
 
-  // TODO: evaluate to input form and go to page
-  // await page.evaluate(() => {})
+      return await Promise.all(
+        items.map(async item => {
+          const { dataset } = item
+          return {
+            hotelid: parseInt(dataset.hotelid, 10),
+            stars: parseInt(dataset.class, 10),
+            score: parseFloat(dataset.score),
+          }
+        }),
+      )
+    })
 
-  // Still figuring out the loading component
+    console.dir(res)
 
-  await browser.close()
+    // await page.screenshot({ path: 'sc.png' })
+  } catch (err) {
+    return context.fail(err)
+  } finally {
+    if (browser !== null) {
+      await browser.close()
+    }
+  }
 
   // https://googleapis.dev/nodejs/storage/latest/File.html#save
   // Disable resumable!!
 
   console.log(data)
   console.log('Done!')
+
+  return context.succeed()
 }
--- scripts/exec-bb.ts
+++ scripts/exec-bb.ts
@@ -1,3 +1,13 @@
 import { scrapeBookingHotels } from '../functions/scrape-booking-hotels'
 
-scrapeBookingHotels({ a: 'asd', b: 12 }, {})
+scrapeBookingHotels(
+  { a: 'asd', b: 12 },
+  {
+    fail(err: any) {
+      console.error(err)
+    },
+    succeed(res: any) {
+      console.log(res)
+    },
+  },
+)
