a22539df46a67f7dab11244f03816a1d2ec08ba0
DaRekaze <darekaze@dar>
2020-01-27 13:23:49 +0800

    done storage trigger fucntion

--- functions/storage-trigger-review/index.ts
+++ functions/storage-trigger-review/index.ts
@@ -2,13 +2,34 @@
 import { PubSub } from '@google-cloud/pubsub'
 import { Storage } from '@google-cloud/storage'
 
-// see https://cloud.google.com/storage/docs/json_api/v1/objects#resource
-export const storageTriggerReview = async (data: any) => {
+// See https://cloud.google.com/storage/docs/json_api/v1/objects#resource
+interface GCSMessage {
+  name: string
+  bucket: string
+}
+
+export const storageTriggerReview = async (data: GCSMessage) => {
+  const pubSubClient = new PubSub()
   const storage = new Storage()
   const file = storage.bucket(data.bucket).file(data.name)
+  const reviewTopic = pubSubClient.topic('SCRAPE_BOOKING_REVIEWS')
 
   const buf = (await file.download())[0]
-  const hotelList = JSON.parse(buf.toString())
+  const hotelList: any[] = JSON.parse(buf.toString())
 
-  console.log(hotelList)
+  // Trigger pubsub for review
+  await Promise.all(
+    hotelList.map(async item => {
+      const dataBuffer = Buffer.from(JSON.stringify({ url: item.url }), 'utf8')
+
+      try {
+        const messageId = await reviewTopic.publish(dataBuffer)
+        console.log(`Message ${messageId} published.`)
+      } catch (err) {
+        console.error(err)
+      }
+    }),
+  )
+
+  return console.log('All Done!')
 }
