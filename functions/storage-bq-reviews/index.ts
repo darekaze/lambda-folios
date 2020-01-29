e75d586f53ea9fa64a4931708d990f1440fa8230
DaRekaze <darekaze@dar>
2020-01-30 02:22:57 +0800

    add reviews to bigquery

--- functions/storage-bq-reviews/index.ts
+++ functions/storage-bq-reviews/index.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 import { Storage } from '@google-cloud/storage'
+import { BigQuery } from '@google-cloud/bigquery'
 
 // See https://cloud.google.com/storage/docs/json_api/v1/objects#resource
 interface GCSMessage {
@@ -7,31 +8,47 @@ interface GCSMessage {
   bucket: string
 }
 
-interface Review {
-  hotel_name: string
-  hotel_address: string
-  avg_score: number
-  total_review_count: number
-  useful_review_count: number
-  review_date: string
-  reviewer_nationality: string
-  reviewer_score: number
-  title: string
-  positive: string
-  negative: string
-  stayed_room: string
-  stayed_night: number
-  scraped_at: string
-}
-
 export const storageBQReviews = async (data: GCSMessage) => {
   const storage = new Storage()
+  const bigquery = new BigQuery()
+
+  const [year, month] = data.name.split('_')[0].split('-')
+
   const file = storage.bucket(data.bucket).file(data.name)
+  const dataset = bigquery.dataset('booking_hotel_reviews')
+  const tableId = `reviews_${year}${month}`
+  const metadata = {
+    sourceFormat: 'CSV',
+    skipLeadingRows: 1,
+    schema: {
+      fields: [
+        { name: 'hotel_name', type: 'STRING' },
+        { name: 'hotel_address', type: 'STRING' },
+        { name: 'avg_score', type: 'FLOAT' },
+        { name: 'total_review_count', type: 'INTEGER' },
+        { name: 'useful_review_count', type: 'INTEGER' },
+        { name: 'review_date', type: 'DATE' },
+        { name: 'reviewer_nationality', type: 'STRING' },
+        { name: 'reviewer_score', type: 'FLOAT' },
+        { name: 'title', type: 'STRING' },
+        { name: 'positive', type: 'STRING' },
+        { name: 'negative', type: 'STRING' },
+        { name: 'stayed_room', type: 'STRING' },
+        { name: 'stayed_night', type: 'INTEGER' },
+        { name: 'scraped_at', type: 'DATE' },
+      ],
+    },
+  }
 
-  const buf = (await file.download())[0]
-  const reviewList: Review[] = JSON.parse(buf.toString())
+  // Load data from GCS file into the table
+  const [job] = await dataset.table(tableId).load(file, metadata)
 
-  // TODO: insert data into big query according to month?
+  // load() waits for the job to finish
+  console.log(`Job ${job.id} completed.`)
 
-  return console.log('All Done!')
+  // Check the job's status for errors
+  const { errors } = job.status
+  if (errors && errors.length > 0) {
+    throw errors
+  }
 }
